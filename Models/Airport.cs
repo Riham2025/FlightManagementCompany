19f58df01aed8ef24997c3e823256f6abcbd2d35
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 10:52:20 +0400

    City where the airport is located

--- Models/Airport.cs
+++ Models/Airport.cs
@@ -15,7 +15,7 @@ namespace FlightManagementCompany.Models
         [Key] public int AirportId { get; set; } // Unique identifier for the airport
         [Required, StringLength(3)] public string IATA { get; set; } = null!; // IATA code (3-letter code) for the airport
         [Required] public string Name { get; set; } = null!; // Name of the airport
-        [Required] public string City { get; set; } = null!;
+        [Required] public string City { get; set; } = null!; // City where the airport is located
         [Required] public string Country { get; set; } = null!;
         public string? TimeZone { get; set; }
     }
