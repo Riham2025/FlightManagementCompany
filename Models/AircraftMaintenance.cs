900b5e4c3c961ef77cfab63f73da725d6a69a533
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 19:53:31 +0400

    Type of maintenance performed (e.g., "A-Check", "B-Check", "C-Check", "D-Check")

--- Models/AircraftMaintenance.cs
+++ Models/AircraftMaintenance.cs
@@ -14,7 +14,7 @@ namespace FlightManagementCompany.Models
         [Key] public int MaintenanceId { get; set; } // Unique identifier for the maintenance record
         [ForeignKey(nameof(Aircraft))] public int AircraftId { get; set; } // Identifier for the aircraft associated with this maintenance record
         public DateTime MaintenanceDate { get; set; } // Date when the maintenance was performed, defaulting to the current UTC time
-        [Required] public string Type { get; set; } = "A-Check";
+        [Required] public string Type { get; set; } = "A-Check"; // Type of maintenance performed (e.g., "A-Check", "B-Check", "C-Check", "D-Check")
         public string? Notes { get; set; }
         public Aircraft Aircraft { get; set; } = null!;
     }
