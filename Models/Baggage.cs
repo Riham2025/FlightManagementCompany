dd27c051d1cd85ea59a365acf8e771b94f690192
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 16:06:11 +0400

    Represents baggage associated with a ticket.

--- Models/Baggage.cs
+++ Models/Baggage.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Models
 {
-    internal class Baggage
+    public class Baggage
     {
+
+        // Represents baggage associated with a ticket.
+        [Key] public int BaggageId { get; set; }
+        [ForeignKey(nameof(Ticket))] public int TicketId { get; set; }
+        [Column(TypeName = "decimal(10,2)")] public decimal WeightKg { get; set; }
+        [StringLength(20)] public string? TagNumber { get; set; }
     }
 }
