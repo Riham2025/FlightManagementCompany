779d9e73ec4409f55a039d8bde4b9e8fc88c7aa6
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 20:58:01 +0400

    DbSets

--- FlightDbContext.cs
+++ FlightDbContext.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlightManagementCompany.Models;
 
 namespace FlightManagementCompany
 {
-     public class FlightDbContext
+     public class FlightDbContext : DbContext
     {
+
+        // DbSets
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Aircraft> Aircraft { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Baggage> Baggage { get; set; }
+        public DbSet<CrewMember> CrewMembers { get; set; }
+        public DbSet<FlightCrew> FlightCrew { get; set; }
+        public DbSet<AircraftMaintenance> AircraftMaintenance { get; set; }
     }
 }
