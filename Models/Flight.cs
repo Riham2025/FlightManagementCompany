9c0546234d0b380a4520c83057fbd2b5c9906130
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 15:30:38 +0400

    Represents a flight with its details

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,13 @@ namespace FlightManagementCompany.Models
 {
     public class Flight
     {
+
+        // Represents a flight with its details.
+        [Key] public int FlightId { get; set; }
+        [Required, StringLength(10)] public string FlightNumber { get; set; } = null!;
+        public DateTime DepartureUtc { get; set; }
+        public DateTime ArrivalUtc { get; set; }
+        [Required] public string Status { get; set; } = "Scheduled";
 
     }
 }
