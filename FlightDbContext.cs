0a0a34d281b733c79f4f673219c8f7976e6e7150
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 20:59:07 +0400

    Represents a collection of flights in the database

--- FlightDbContext.cs
+++ FlightDbContext.cs
@@ -14,7 +14,7 @@ namespace FlightManagementCompany
         public DbSet<Airport> Airports { get; set; } // Represents a collection of airports in the database
         public DbSet<Aircraft> Aircraft { get; set; } // Represents a collection of aircraft in the database
         public DbSet<Route> Routes { get; set; } // Represents a collection of flight routes in the database
-        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Flight> Flights { get; set; } // Represents a collection of flights in the database
         public DbSet<Passenger> Passengers { get; set; }
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<Ticket> Tickets { get; set; }
