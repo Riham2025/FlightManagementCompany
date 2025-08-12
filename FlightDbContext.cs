9a472957437df1262eb157f5ef4d1901b1425bbf
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 21:25:59 +0400

    Represents the Ticket entity

--- FlightDbContext.cs
+++ FlightDbContext.cs
@@ -89,7 +89,7 @@ namespace FlightManagementCompany
             // Ticket
             mb.Entity<Ticket>() // Represents the Ticket entity
               .Property(t => t.Fare).HasColumnType("decimal(10,2)"); // Ensure Fare is a decimal with 10 digits total and 2 decimal places
-            mb.Entity<Ticket>()
+            mb.Entity<Ticket>() // Represents the Ticket entity
               .HasOne(t => t.Booking).WithMany(b => b.Tickets)
               .HasForeignKey(t => t.BookingId);
             mb.Entity<Ticket>()
