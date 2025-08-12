5c10bc192bd41e1929680b80d0c67d1625ccf07e
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 15:45:54 +0400

    Represents a booking made by a passenger for a flight

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Models
 {
-    internal class Booking
+    public  class Booking
     {
+        // Represents a booking made by a passenger for a flight.
+        [Key] public int BookingId { get; set; }
+        [Required, StringLength(12)] public string BookingRef { get; set; } = null!;
+        public DateTime BookingDate { get; set; } = DateTime.UtcNow;
+        [Required] public string Status { get; set; } = "Confirmed";
     }
 }
