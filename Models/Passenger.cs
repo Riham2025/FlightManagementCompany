e061cde8c21d799eedd40eab1640cbd941d5438b
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 15:39:33 +0400

    Represents a passenger with their details

--- Models/Passenger.cs
+++ Models/Passenger.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Models
 {
-    internal class Passenger
+    public class Passenger
     {
+        // Represents a passenger with their details.
+        [Key] public int PassengerId { get; set; }
+        [Required] public string FullName { get; set; } = null!;
+        [Required, StringLength(20)] public string PassportNo { get; set; } = null!;
+        public string Nationality { get; set; } = "US";
+        public DateTime DOB { get; set; }
     }
 }
