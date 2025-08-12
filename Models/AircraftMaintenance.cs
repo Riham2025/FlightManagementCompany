61da5e7bb5fcd0c43d725905196a5fdfef73b6a4
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 19:52:54 +0400

    Identifier for the aircraft associated with this maintenance record

--- Models/AircraftMaintenance.cs
+++ Models/AircraftMaintenance.cs
@@ -12,7 +12,7 @@ namespace FlightManagementCompany.Models
     {
         // Represents maintenance records for an aircraft.
         [Key] public int MaintenanceId { get; set; } // Unique identifier for the maintenance record
-        [ForeignKey(nameof(Aircraft))] public int AircraftId { get; set; }
+        [ForeignKey(nameof(Aircraft))] public int AircraftId { get; set; } // Identifier for the aircraft associated with this maintenance record
         public DateTime MaintenanceDate { get; set; }
         [Required] public string Type { get; set; } = "A-Check";
         public string? Notes { get; set; }
