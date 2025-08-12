7ea6ef600f29672c8706f26b046add2d001e0866
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 15:57:37 +0400

    Represents a ticket issued for a flight booking.

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,9 @@ namespace FlightManagementCompany.Models
 {
     public class Ticket
     {
+        // Represents a ticket issued for a flight booking.
+        [Key] public int TicketId { get; set; }
+        [ForeignKey(nameof(Booking))] public int BookingId { get; set; }
+        [ForeignKey(nameof(Flight))] public int FlightId { get; set; }
     }
 }
