c6ebf6311305ceefae3cc9e65461d5aec2384293
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 19:32:52 +0400

    Represents a crew member working on a flight.

--- Models/CrewMember.cs
+++ Models/CrewMember.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace FlightManagementCompany.Models
 {
     public class CrewMember
     {
+        // Represents a crew member working on a flight.
+        [Key] public int CrewId { get; set; }
+        [Required] public string FullName { get; set; } = null!;
+        [Required] public string Role { get; set; } = "FlightAttendant"; // or enum
+        public string? LicenseNo { get; set; }
+
+
     }
 }
