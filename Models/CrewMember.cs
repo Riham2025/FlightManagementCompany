5425acea27a6ba6e4407c31febc6ae0c6bf7ad1a
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 19:35:10 +0400

    Role of the crew member (e.g., "Pilot", "Flight Attendant", "Engineer")

--- Models/CrewMember.cs
+++ Models/CrewMember.cs
@@ -12,7 +12,7 @@ namespace FlightManagementCompany.Models
         // Represents a crew member working on a flight.
         [Key] public int CrewId { get; set; } // Unique identifier for the crew member
         [Required] public string FullName { get; set; } = null!; // Full name of the crew member
-        [Required] public string Role { get; set; } = "FlightAttendant"; // or enum
+        [Required] public string Role { get; set; } = "FlightAttendant"; // or enum // Role of the crew member (e.g., "Pilot", "Flight Attendant", "Engineer")
         public string? LicenseNo { get; set; }
 
 
