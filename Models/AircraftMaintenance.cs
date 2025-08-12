44e3e4fbea2224f17e0bee8a44862d90a360683f
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 19:53:54 +0400

    Additional notes or comments about the maintenance performed

--- Models/AircraftMaintenance.cs
+++ Models/AircraftMaintenance.cs
@@ -15,7 +15,7 @@ namespace FlightManagementCompany.Models
         [ForeignKey(nameof(Aircraft))] public int AircraftId { get; set; } // Identifier for the aircraft associated with this maintenance record
         public DateTime MaintenanceDate { get; set; } // Date when the maintenance was performed, defaulting to the current UTC time
         [Required] public string Type { get; set; } = "A-Check"; // Type of maintenance performed (e.g., "A-Check", "B-Check", "C-Check", "D-Check")
-        public string? Notes { get; set; }
+        public string? Notes { get; set; } // Additional notes or comments about the maintenance performed
         public Aircraft Aircraft { get; set; } = null!;
     }
 }
