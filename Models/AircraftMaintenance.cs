c1fc06f70b5940c70553520b2a9deaa5dff99979
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 19:52:24 +0400

    Represents maintenance records for an aircraft.

--- Models/AircraftMaintenance.cs
+++ Models/AircraftMaintenance.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace FlightManagementCompany.Models
 {
     public class AircraftMaintenance
     {
+        // Represents maintenance records for an aircraft.
+        [Key] public int MaintenanceId { get; set; }
+        [ForeignKey(nameof(Aircraft))] public int AircraftId { get; set; }
+        public DateTime MaintenanceDate { get; set; }
+        [Required] public string Type { get; set; } = "A-Check";
+        public string? Notes { get; set; }
+        public Aircraft Aircraft { get; set; } = null!;
     }
 }
