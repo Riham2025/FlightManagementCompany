615fad75e305de2ea701c3035d84df233ceb11cf
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-12 16:07:10 +0400

    Baggage tag number (optional, can be null)

--- Models/Baggage.cs
+++ Models/Baggage.cs
@@ -15,6 +15,6 @@ namespace FlightManagementCompany.Models
         [Key] public int BaggageId { get; set; } // Unique identifier for the baggage
         [ForeignKey(nameof(Ticket))] public int TicketId { get; set; } // Identifier for the ticket associated with this baggage
         [Column(TypeName = "decimal(10,2)")] public decimal WeightKg { get; set; } // Weight of the baggage in kilograms
-        [StringLength(20)] public string? TagNumber { get; set; }
+        [StringLength(20)] public string? TagNumber { get; set; } // Baggage tag number (optional, can be null)
     }
 }
