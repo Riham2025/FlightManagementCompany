701883b1ca45bdaeb4423a0c8e34a3cac232c5fc
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 15:59:15 +0400

    Delete a booking by its unique identifier

--- Repository/IBookingRepository.cs
+++ Repository/IBookingRepository.cs
@@ -5,7 +5,7 @@ namespace FlightManagementCompany.Repository
     public interface IBookingRepository // Represents an interface for managing booking entities in the flight management system.
     {
         void Add(Booking entity); // Stage add new booking
-        void Delete(int id);
+        void Delete(int id); // Delete a booking by its unique identifier
         List<Booking> GetAll();
         Booking? GetById(int id);
         void Save();
