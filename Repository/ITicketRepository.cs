4ddb9453aebdd0f992d5ce14a384d9439fcc428d
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 19:38:35 +0400

    Deletes a ticket entity by its unique identifier.

--- Repository/ITicketRepository.cs
+++ Repository/ITicketRepository.cs
@@ -5,7 +5,7 @@ namespace FlightManagementCompany.Repository
     public interface ITicketRepository// Represents an interface for managing ticket entities in the flight management system.
     {
         void Add(Ticket entity);// Adds a new ticket entity to the repository.
-        void Delete(int id);
+        void Delete(int id); // Deletes a ticket entity by its unique identifier.
         List<Ticket> GetAll();
         Ticket? GetById(int id);
         void Save();
