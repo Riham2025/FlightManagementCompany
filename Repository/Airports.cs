26ed6be9b15cdfda9b0a6a798a187dc20b6256c0
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 11:03:39 +0400

    Adds a new airport entity to the repository

--- Repository/Airports.cs
+++ Repository/Airports.cs
@@ -8,14 +8,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FlightManagementCompany.Repository
 {
-    public class Airports
+    public class Airports : IAirports 
     {
         // Represents a repository for managing airports in the flight management system.
         private readonly FlightDbContext _ctx; // Database context for accessing airport data
         public Airports(FlightDbContext ctx) => _ctx = ctx; // Constructor to initialize the repository with a database context
 
 
-       
+
         public List<Airport> GetAll() => // Retrieves all airports from the database.
             _ctx.Airports.AsNoTracking().ToList();// AsNoTracking() is used to improve performance by not tracking changes to the entities.
 
@@ -31,7 +31,7 @@ namespace FlightManagementCompany.Repository
 
         public void Delete(int id) // Deletes an airport by its unique identifier.
         {
-            
+
             var e = _ctx.Airports.Find(id); // Find the airport entity by its primary key (AirportId).
             if (e != null) _ctx.Airports.Remove(e);
         }
---
+++ Repository/IAirports.cs
@@ -0,0 +1,16 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    
+    public interface IAirports 
+    {
+        void Add(Airport entity); // Adds a new airport entity to the repository.
+        void Delete(int id);
+        List<Airport> GetAll();
+        Airport? GetByIata(string iata);
+        Airport? GetById(int id);
+        void Save();
+        void Update(Airport entity);
+    }
+}
