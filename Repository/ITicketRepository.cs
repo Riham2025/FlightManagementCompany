23f0518d35000cdce970efba90d33c300e4b673f
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 19:38:15 +0400

    Adds a new ticket entity to the repository.

--- Repository/ITicketRepository.cs
+++ Repository/ITicketRepository.cs
@@ -4,7 +4,7 @@ namespace FlightManagementCompany.Repository
 {
     public interface ITicketRepository// Represents an interface for managing ticket entities in the flight management system.
     {
-        void Add(Ticket entity);
+        void Add(Ticket entity);// Adds a new ticket entity to the repository.
         void Delete(int id);
         List<Ticket> GetAll();
         Ticket? GetById(int id);
