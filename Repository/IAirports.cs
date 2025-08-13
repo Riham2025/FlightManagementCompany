280b5bfc1371ce2c501302fb32d7a59f95fee3dd
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 11:05:17 +0400

    Saves changes made to the repository.

--- Repository/IAirports.cs
+++ Repository/IAirports.cs
@@ -10,7 +10,7 @@ namespace FlightManagementCompany.Repository
         List<Airport> GetAll(); // Retrieves all airport entities from the repository.
         Airport? GetByIata(string iata); // Retrieves an airport entity by its IATA code.
         Airport? GetById(int id); // Retrieves an airport entity by its unique identifier.
-        void Save();
+        void Save(); // Saves changes made to the repository.
         void Update(Airport entity);
     }
 }
