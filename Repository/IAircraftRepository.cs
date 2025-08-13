9e67e8f68b5f0360cccd0fcf04ba3840d5f93d8f
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 11:43:11 +0400

    Deletes an aircraft entity by its unique identifier

--- Repository/IAircraftRepository.cs
+++ Repository/IAircraftRepository.cs
@@ -5,7 +5,7 @@ namespace FlightManagementCompany.Repository
     public interface IAircraftRepository // Represents an interface for managing aircraft entities in the flight management system.
     {
         void Add(Aircraft entity); // Adds a new aircraft entity to the repository.
-        void Delete(int id);
+        void Delete(int id); // Deletes an aircraft entity by its unique identifier.
         List<Aircraft> GetAll();
         Aircraft? GetById(int id);
         void Update(Aircraft entity);
