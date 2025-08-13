211574889ddf7da63617afda832f4cdb74615cb1
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 19:39:14 +0400

    Retrieves a ticket entity by its unique identifier.

--- Repository/ITicketRepository.cs
+++ Repository/ITicketRepository.cs
@@ -7,7 +7,7 @@ namespace FlightManagementCompany.Repository
         void Add(Ticket entity);// Adds a new ticket entity to the repository.
         void Delete(int id); // Deletes a ticket entity by its unique identifier.
         List<Ticket> GetAll();// Retrieves all ticket entities from the repository.
-        Ticket? GetById(int id);
+        Ticket? GetById(int id);// Retrieves a ticket entity by its unique identifier.
         void Save();
         void Update(Ticket entity);
     }
