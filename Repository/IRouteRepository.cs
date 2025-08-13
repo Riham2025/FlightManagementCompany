e834abdd1a1e701cc51f023359020b90af62c374
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 12:09:49 +0400

    Delete a route by its unique identifier

--- Repository/IRouteRepository.cs
+++ Repository/IRouteRepository.cs
@@ -5,7 +5,7 @@ namespace FlightManagementCompany.Repository
     public interface IRouteRepository // Represents an interface for managing flight routes in the flight management system.
     {
         void Add(Route entity); // Stage add new route
-        void Delete(int id);
+        void Delete(int id); // Delete a route by its unique identifier
         List<Route> GetAll();
         Route? GetById(int id);
         void Save();
