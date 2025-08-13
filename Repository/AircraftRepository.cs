e69c2f96e6e2dd6cd09423a715ff6c0d49c695f0
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 11:31:55 +0400

    Represents a repository for managing aircraft entities in the flight management system

---
+++ Repository/AircraftRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightManagementCompany.Repository
+{
+    public class AircraftRepository : IAircraftRepository // Represents a repository for managing aircraft entities in the flight management system.
+    {
+
+        private readonly FlightDbContext _ctx;
+
+        public AircraftRepository(FlightDbContext ctx) // ctor injection
+        {
+            _ctx = ctx;
+        }
+
+        // Get all aircraft (MODEL), not a repository type
+        public List<FlightManagementCompany.Models.Aircraft> GetAll()
+        {
+            return _ctx.Aircraft               // DbSet<Models.Aircraft>
+                       .AsNoTracking()
+                       .ToList();              // List<Models.Aircraft>
+        }
+
+        // Get by id
+        public FlightManagementCompany.Models.Aircraft? GetById(int id)
+        {
+            return _ctx.Aircraft.Find(id);
+        }
+
+        // Add new aircraft
+        public void Add(FlightManagementCompany.Models.Aircraft entity)
+        {
+            _ctx.Aircraft.Add(entity);
+            _ctx.SaveChanges();
+        }
+
+        // Update existing aircraft
+        public void Update(FlightManagementCompany.Models.Aircraft entity)
+        {
+            _ctx.Aircraft.Update(entity);
+            _ctx.SaveChanges();
+        }
+
+        // Delete by id
+        public void Delete(int id)
+        {
+            var e = _ctx.Aircraft.Find(id);
+            if (e != null)
+            {
+                _ctx.Aircraft.Remove(e);
+                _ctx.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ Repository/IAircraftRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IAircraftRepository
+    {
+        void Add(Aircraft entity);
+        void Delete(int id);
+        List<Aircraft> GetAll();
+        Aircraft? GetById(int id);
+        void Update(Aircraft entity);
+    }
+}
