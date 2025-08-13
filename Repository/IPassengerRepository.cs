e42d981315f3535de06198c5b8c0b409bdeaccef
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 15:48:52 +0400

    Adds a new passenger entity to the repository.

---
+++ Repository/IPassengerRepository.cs
@@ -0,0 +1,14 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IPassengerRepository // Represents an interface for managing passenger entities in the flight management system.
+    {
+        void Add(Passenger entity); // Adds a new passenger entity to the repository.
+        void Delete(int id);
+        List<Passenger> GetAll();
+        Passenger? GetById(int id);
+        void Save();
+        void Update(Passenger entity);
+    }
+}
