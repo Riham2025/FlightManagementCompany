b2d8e504399067d9a3be8fd8d6275435fad1e796
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 11:42:58 +0400

    Adds a new aircraft entity to the repository.

--- Repository/IAircraftRepository.cs
+++ Repository/IAircraftRepository.cs
@@ -4,7 +4,7 @@ namespace FlightManagementCompany.Repository
 {
     public interface IAircraftRepository // Represents an interface for managing aircraft entities in the flight management system.
     {
-        void Add(Aircraft entity);
+        void Add(Aircraft entity); // Adds a new aircraft entity to the repository.
         void Delete(int id);
         List<Aircraft> GetAll();
         Aircraft? GetById(int id);
