2398ff834a9ea7b7a10b9b1d9bd34e643756bf05
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 20:05:50 +0400

    Assign the provided context to the private field

--- Repository/BaggageRepository.cs
+++ Repository/BaggageRepository.cs
@@ -15,7 +15,7 @@ namespace FlightManagementCompany.Repository
 
         public BaggageRepository(FlightDbContext ctx)// Constructor to initialize the repository with a database context
         {
-            _ctx = ctx;
+            _ctx = ctx; // Assign the provided context to the private field
         }
 
         public List<Baggage> GetAll()
