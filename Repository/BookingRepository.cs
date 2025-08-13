c7bf56b8907c792d9a8d4758bc4ac222415a1739
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 15:49:09 +0400

    Deletes a passenger entity by its unique identifier.

--- Repository/BookingRepository.cs
+++ Repository/BookingRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompany.Repository
 {
-    public class BookingRepository
+    public class BookingRepository 
     {
     }
 }
--- Repository/IPassengerRepository.cs
+++ Repository/IPassengerRepository.cs
@@ -5,7 +5,7 @@ namespace FlightManagementCompany.Repository
     public interface IPassengerRepository // Represents an interface for managing passenger entities in the flight management system.
     {
         void Add(Passenger entity); // Adds a new passenger entity to the repository.
-        void Delete(int id);
+        void Delete(int id); // Deletes a passenger entity by its unique identifier.
         List<Passenger> GetAll();
         Passenger? GetById(int id);
         void Save();
