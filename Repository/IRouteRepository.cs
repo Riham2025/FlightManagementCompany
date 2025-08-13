efc9a2eae944d4c4152b60943cf36a78fbffd12d
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 12:09:35 +0400

    Stage add new route

--- Repository/IRouteRepository.cs
+++ Repository/IRouteRepository.cs
@@ -4,7 +4,7 @@ namespace FlightManagementCompany.Repository
 {
     public interface IRouteRepository // Represents an interface for managing flight routes in the flight management system.
     {
-        void Add(Route entity);
+        void Add(Route entity); // Stage add new route
         void Delete(int id);
         List<Route> GetAll();
         Route? GetById(int id);
