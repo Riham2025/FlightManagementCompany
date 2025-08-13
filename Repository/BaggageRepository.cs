2f1e08c7bb2cab8870fb9782789f07849c26907d
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 20:04:41 +0400

    Represents a repository for managing baggage entities in the flight management system.

--- Repository/BaggageRepository.cs
+++ Repository/BaggageRepository.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlightManagementCompany.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlightManagementCompany.Repository
 {
-    public class BaggageRepository
+    public class BaggageRepository : IBaggageRepository// Represents a repository for managing baggage entities in the flight management system.
     {
+
+        private readonly FlightDbContext _ctx;
+
+        public BaggageRepository(FlightDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public List<Baggage> GetAll()
+        {
+            return _ctx.Baggage
+                       .Include(b => b.Passenger)
+                       .AsNoTracking()
+                       .ToList();
+        }
+
+        public Baggage? GetById(int id)
+        {
+            return _ctx.Baggage
+                       .Include(b => b.Passenger)
+                       .FirstOrDefault(b => b.BaggageId == id);
+        }
+
+        public void Add(Baggage entity)
+        {
+            _ctx.Baggage.Add(entity);
+        }
+
+        public void Update(Baggage entity)
+        {
+            _ctx.Baggage.Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var e = _ctx.Baggage.Find(id);
+            if (e != null) _ctx.Baggage.Remove(e);
+        }
+
+        public void Save()
+        {
+            _ctx.SaveChanges();
+        }
     }
 }
---
+++ Repository/IBaggageRepository.cs
@@ -0,0 +1,14 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Repository
+{
+    public interface IBaggageRepository
+    {
+        void Add(Baggage entity);
+        void Delete(int id);
+        List<Baggage> GetAll();
+        Baggage? GetById(int id);
+        void Save();
+        void Update(Baggage entity);
+    }
+}
