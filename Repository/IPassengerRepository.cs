5645a697ba97fe3efabd98dd5a5ee80cbd43f5a5
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 15:50:04 +0400

    Saves changes made to the repository.

--- Repository/IPassengerRepository.cs
+++ Repository/IPassengerRepository.cs
@@ -8,7 +8,7 @@ namespace FlightManagementCompany.Repository
         void Delete(int id); // Deletes a passenger entity by its unique identifier.
         List<Passenger> GetAll(); //Retrieves all passenger entities from the repository.
         Passenger? GetById(int id); // Retrieves a passenger entity by its unique identifier.
-        void Save();
+        void Save(); // Saves changes made to the repository.
         void Update(Passenger entity);
     }
 }
