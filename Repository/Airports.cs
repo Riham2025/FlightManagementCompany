710e2d738d430bfb0427abdbcf0fc20ff8c16dac
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-13 10:17:00 +0400

    Retrieves all airports from the database

--- Repository/Airports.cs
+++ Repository/Airports.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlightManagementCompany.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlightManagementCompany.Repository
 {
@@ -11,5 +13,9 @@ namespace FlightManagementCompany.Repository
         // Represents a repository for managing airports in the flight management system.
         private readonly FlightDbContext _ctx; // Database context for accessing airport data
         public Airports(FlightDbContext ctx) => _ctx = ctx; // Constructor to initialize the repository with a database context
+
+        
+        public List<Airport> GetAll() => // Retrieves all airports from the database.
+            _ctx.Airports.AsNoTracking().ToList();
     }
 }
