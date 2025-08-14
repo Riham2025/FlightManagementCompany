7a47a66fff360bab7f8d2e8b67e5e5d12754fad0
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-14 11:23:04 +0400

    Saves changes made to the database context

--- Repository/IAircraftMaintenanceRepository.cs
+++ Repository/IAircraftMaintenanceRepository.cs
@@ -10,7 +10,7 @@ namespace FlightManagementCompany.Repository
         List<AircraftMaintenance> GetByAircraft(int aircraftId); // Retrieve all maintenance records for a specific aircraft
         AircraftMaintenance? GetById(int id);// Retrieve a single aircraft maintenance record by its unique identifier
         List<AircraftMaintenance> GetRecent(int days); // Retrieve maintenance records performed in the last N days
-        void Save();
+        void Save(); // Saves changes made to the database context
         void Update(AircraftMaintenance entity);
     }
 }
