885147f4491a5181a923b01d1ef78d11d362991b
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-14 11:22:12 +0400

    Retrieve a single aircraft maintenance record by its unique identifier

--- Repository/IAircraftMaintenanceRepository.cs
+++ Repository/IAircraftMaintenanceRepository.cs
@@ -8,7 +8,7 @@ namespace FlightManagementCompany.Repository
         void Delete(int id); // Delete an aircraft maintenance record by its unique identifier
         List<AircraftMaintenance> GetAll(); // Retrieve all aircraft maintenance records, including linked Aircraft
         List<AircraftMaintenance> GetByAircraft(int aircraftId); // Retrieve all maintenance records for a specific aircraft
-        AircraftMaintenance? GetById(int id);
+        AircraftMaintenance? GetById(int id);// Retrieve a single aircraft maintenance record by its unique identifier
         List<AircraftMaintenance> GetRecent(int days);
         void Save();
         void Update(AircraftMaintenance entity);
