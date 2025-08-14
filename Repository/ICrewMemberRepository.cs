1398aa0bdad663d3f27abd728a1dd207ab287822
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-14 09:54:03 +0400

    Delete a crew member by its unique identifier

--- Repository/ICrewMemberRepository.cs
+++ Repository/ICrewMemberRepository.cs
@@ -5,7 +5,7 @@ namespace FlightManagementCompany.Repository
     public interface ICrewMemberRepository // Represents a repository for managing crew member entities in the flight management system.
     {
         void Add(CrewMember entity);// Stage add new crew member
-        void Delete(int id);
+        void Delete(int id); // Delete a crew member by its unique identifier
         List<CrewMember> GetAll();
         CrewMember? GetById(int id);
         void Save();
