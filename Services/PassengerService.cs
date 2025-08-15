3abd61dee56509d8a0d6c8bc377fb548d1616307
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 16:32:08 +0400

    create class PassengerService

---
+++ Services/PassengerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightManagementCompany.Models;
+using FlightManagementCompany.Repository;
+
+namespace FlightManagementCompany.Services
+{
+    public class PassengerService
+    {
+
+
+    }
+}
