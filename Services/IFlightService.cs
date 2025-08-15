89200ab37e79610c9694ac88f7e15a4df2f51b27
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 16:26:04 +0400

    Represents a service for managing flight operations in the flight management system

---
+++ Services/IFlightService.cs
@@ -0,0 +1,14 @@
+﻿using FlightManagementCompany.Models;
+
+namespace FlightManagementCompany.Services
+{
+    public interface IFlightService // Represents a service for managing flight operations in the flight management system.
+    {
+        bool Create(string flightNumber, int routeId, int aircraftId, DateTime depUtc, DateTime arrUtc, out string error);
+        bool Delete(int id, out string error);
+        List<Flight> GetAll();
+        List<Flight> GetByDateRange(DateTime fromUtc, DateTime toUtc);
+        Flight? GetById(int id);
+        bool Update(Flight flight, out string error);
+    }
+}
