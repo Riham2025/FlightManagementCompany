5d6ff0c33562cde48428351f856a910433219e9e
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 14:58:47 +0400

    Retrieves all airports from the repository

--- Services/IAirportService.cs
+++ Services/IAirportService.cs
@@ -6,7 +6,7 @@ namespace FlightManagementCompany.Services
     {
         bool Create(string iata, string name, string city, string country, string timeZone, out string error); // Creates a new airport with the specified details and validates the input.
         bool Delete(int id, out string error); // Deletes an airport by its unique identifier and validates the input.
-        List<Airport> GetAll();
+        List<Airport> GetAll(); // Retrieves all airports from the repository.
         Airport? GetById(int id);
         bool Update(Airport airport, out string error);
     }
