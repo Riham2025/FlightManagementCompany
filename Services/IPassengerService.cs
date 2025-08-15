8e403c34530b1f337fea2f36e9c308acfe51483e
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 16:47:18 +0400

    Retrieves all passengers from the repository, including their details such as full name, passport number, and email

--- Services/IPassengerService.cs
+++ Services/IPassengerService.cs
@@ -5,7 +5,7 @@ namespace FlightManagementCompany.Services
     public interface IPassengerService // Represents a service for managing passenger operations in the flight management system.
     {
         bool Delete(int id, out string error); // Deletes a passenger by their unique identifier and validates the input.
-        List<Passenger> GetAll();
+        List<Passenger> GetAll(); // Retrieves all passengers from the repository, including their details such as full name, passport number, and email.
         Passenger? GetById(int id);
         bool Register(string fullName, string passportNo, string email, out string error);
         bool Update(Passenger passenger, out string error);
