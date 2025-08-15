95c0234d0468dd947d1356e340ad543f7ab79f56
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 17:05:33 +0400

    Retrieves all bookings from the repository, including associated passengers and flights

--- Services/IBookingService.cs
+++ Services/IBookingService.cs
@@ -6,7 +6,7 @@ namespace FlightManagementCompany.Services
     {
         bool Cancel(int id, out string error); // Cancels a booking by its unique identifier and validates the input.
         bool Create(int passengerId, int flightId, out string error); // Creates a new booking for a passenger on a specific flight and validates the input.
-        List<Booking> GetAll();
+        List<Booking> GetAll(); // Retrieves all bookings from the repository, including associated passengers and flights.
         Booking? GetById(int id);
     }
 }
