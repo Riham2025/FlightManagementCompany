8f1a5ad90c9e026d6115d460293f1bb27f93c64c
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 14:58:33 +0400

    Deletes an airport by its unique identifier and validates the input

--- Services/IAirportService.cs
+++ Services/IAirportService.cs
@@ -5,7 +5,7 @@ namespace FlightManagementCompany.Services
     public interface IAirportService // Represents a service for managing airport operations in the flight management system.
     {
         bool Create(string iata, string name, string city, string country, string timeZone, out string error); // Creates a new airport with the specified details and validates the input.
-        bool Delete(int id, out string error);
+        bool Delete(int id, out string error); // Deletes an airport by its unique identifier and validates the input.
         List<Airport> GetAll();
         Airport? GetById(int id);
         bool Update(Airport airport, out string error);
