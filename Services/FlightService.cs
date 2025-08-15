7186836aa6c8991ffc3e5620165d210197b21d50
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 16:24:30 +0400

    Initialize error message to empty string.

--- Services/FlightService.cs
+++ Services/FlightService.cs
@@ -56,7 +56,7 @@ namespace FlightManagementCompany.Services
 
         public bool Delete(int id, out string error) // Deletes a flight by its unique identifier and validates the input.
         {
-            error = string.Empty;
+            error = string.Empty; // Initialize error message to empty string.
             _flights.Delete(id); _flights.Save();
             return true;
         }
