e62edb7f7d805509a886a38ec32f7b66112afc4c
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 16:47:37 +0400

    Retrieves a passenger by their unique identifier, including their details such as full name, passport number, and email

--- Services/IPassengerService.cs
+++ Services/IPassengerService.cs
@@ -6,7 +6,7 @@ namespace FlightManagementCompany.Services
     {
         bool Delete(int id, out string error); // Deletes a passenger by their unique identifier and validates the input.
         List<Passenger> GetAll(); // Retrieves all passengers from the repository, including their details such as full name, passport number, and email.
-        Passenger? GetById(int id);
+        Passenger? GetById(int id); // Retrieves a passenger by their unique identifier, including their details such as full name, passport number, and email.
         bool Register(string fullName, string passportNo, string email, out string error);
         bool Update(Passenger passenger, out string error);
     }
