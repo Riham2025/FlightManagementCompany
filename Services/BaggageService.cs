95a9a0e47738267e0e0fa57c0ab657945d6cb6bb
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 20:42:26 +0400

    create class BaggageService

---
+++ Services/BaggageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightManagementCompany.Models;
+using FlightManagementCompany.Repository;
+
+
+namespace FlightManagementCompany.Services
+{
+    public class BaggageService
+    {
+
+
+    }
+}
