f7b50284d631888b3da59042557cab0271ef68e9
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 21:31:50 +0400

    Retrieves a crew member by their unique identifier, including their details such as full name and role

--- Services/ICrewMemberService.cs
+++ Services/ICrewMemberService.cs
@@ -7,7 +7,7 @@ namespace FlightManagementCompany.Services
         bool Create(string fullName, string role, out string error); // Creates a new crew member with the specified full name and role, and validates the input.
         bool Delete(int id, out string error); // Deletes a crew member by their unique identifier and validates the input.
         List<CrewMember> GetAll(); // Retrieves all crew members from the repository, including their details such as full name and role.
-        CrewMember? GetById(int id);
+        CrewMember? GetById(int id); // Retrieves a crew member by their unique identifier, including their details such as full name and role.
         bool Update(CrewMember crew, out string error);
     }
 }
