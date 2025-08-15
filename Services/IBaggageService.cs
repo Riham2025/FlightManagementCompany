45f62d87affeb5c820efddcc8f81d6ab61de286c
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 21:16:51 +0400

    Retrieves a baggage entry by its unique identifier, including associated passenger details

--- Services/IBaggageService.cs
+++ Services/IBaggageService.cs
@@ -7,7 +7,7 @@ namespace FlightManagementCompany.Services
         bool Add(int passengerId, string tagNumber, double weightKg, out string error); // Adds a new baggage entry for a passenger with the specified details and validates the input.
         bool Delete(int id, out string error); // Deletes a baggage entry by its unique identifier and validates the input.
         List<Baggage> GetAll(); // Retrieves all baggage entries from the repository, including associated passengers.
-        Baggage? GetById(int id);
+        Baggage? GetById(int id); // Retrieves a baggage entry by its unique identifier, including associated passenger details.
         bool Update(Baggage baggage, out string error);
     }
 }
