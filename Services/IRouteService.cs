5176b17c00c86a76fd561491b90da1eb05d79a11
Riham2025 <riham91.alsiyabi@gmail.com>
2025-08-15 15:36:48 +0400

    Retrieves a route by its unique identifier, including origin and destination airports

--- Services/IRouteService.cs
+++ Services/IRouteService.cs
@@ -7,7 +7,7 @@ namespace FlightManagementCompany.Services
         bool Create(int originAirportId, int destinationAirportId, out string error); // Creates a new flight route with the specified origin and destination airport IDs and validates the input.
         bool Delete(int id, out string error); // Deletes a flight route by its unique identifier and validates the input.
         List<Route> GetAll(); // Retrieves all routes from the repository, including origin and destination airports.
-        Route? GetById(int id);
+        Route? GetById(int id); // Retrieves a route by its unique identifier, including origin and destination airports.
         bool Update(Route route, out string error);
     }
 }
